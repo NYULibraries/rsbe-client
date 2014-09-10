e23eaa2fa568c7174ee9bc373e367a87f9b8ff9d
jgpawletko <jgpawletko@gmail.com>
2014-09-10 16:51:07 -0400

    add base class

--- lib/rsbe/client/base.rb
+++ lib/rsbe/client/base.rb
@@ -1,6 +1,18 @@
+require 'faraday'
+
 module Rsbe
   module Client
     class Base
+      def initialize
+        @user     = ENV['RSBE_USER']     || 'foo'
+        @password = ENV['RSBE_PASSWORD'] || 'bar'
+        @url      = ENV['RSBE_URL']      || 'http://localhost:3000'
+        @conn     = Faraday.new(url: @url) do |faraday|
+          faraday.response :logger                  # log requests to STDOUT
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+        end
+        @conn.basic_auth(@user, @password)
+      end
     end
   end
 end
