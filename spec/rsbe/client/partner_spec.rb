ed9facac5c12258b2c2a7b71f8a20db996a599fd
jgpawletko <jgpawletko@gmail.com>
2014-09-10 19:20:30 -0400

    Add skeleton examples to Partner spec
    Add skeleton examples to Partner spec showing which methods will be implemented.

--- spec/rsbe/client/partner_spec.rb
+++ spec/rsbe/client/partner_spec.rb
@@ -1,6 +1,13 @@
 describe Rsbe::Client::Partner do
-  context "when created" do
+  describe ".new" do
     subject { Rsbe::Client::Partner.new }
     it      { should be_a(Rsbe::Client::Partner) }
+  end
+
+  describe ".all" do
+  describe ".find"
+  describe "#save" do
+    context "when id is known"
+    context "when id is unknown"
   end
 end
