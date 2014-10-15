dbecb5c4385f4bf578ab290e5373b83d7401b38f
jgpawletko <jgpawletko@gmail.com>
2014-10-15 09:22:02 -0400

    add context levels to Partner#save tests

--- spec/rsbe/client/partner_spec.rb
+++ spec/rsbe/client/partner_spec.rb
@@ -15,33 +15,41 @@ describe Rsbe::Client::Partner do
 
 
   describe "#save" do
-    context "with valid attributes, unknown id", vcr: {cassette_name: 'partner/save-unknown-id'}do
-      let(:partner) { Rsbe::Client::Partner.new(code: 'foo', rel_path: 'a/b/c') }
-      subject { partner }
+    context "a new Partner" do
+      context "with valid attributes" do
+        context "and no id", vcr: {cassette_name: 'partner/save-unknown-id'} do
+          let(:partner) { Rsbe::Client::Partner.new(code: 'foo', rel_path: 'a/b/c') }
+          subject { partner }
 
-      its(:save) { should eq true }
+          its(:save) { should eq true }
 
-      context "after save" do
-        before { partner.save }
-        its(:id)         { should_not be_nil }
-        its(:code)       { should eq 'foo'   }
-        its(:created_at) { should_not be_nil }
-        its(:updated_at) { should_not be_nil }
-      end
-    end
+          context "after save" do
+            before { partner.save }
+            its(:id)         { should_not be_nil }
+            its(:code)       { should eq 'foo'   }
+            its(:created_at) { should_not be_nil }
+            its(:updated_at) { should_not be_nil }
+          end
+        end
 
-    context "with valid attributes, known id", vcr: {cassette_name: 'partner/save-known-id'} do
-      let(:partner) { Rsbe::Client::Partner.new(id: '51213be7-c8de-4e06-8cc2-06bfc82cdd68', code: 'bar', rel_path: 'd/e/f') }
-      subject { partner }
+        context "and a known id", vcr: {cassette_name: 'partner/save-known-id'} do
+          let(:partner) do
+            Rsbe::Client::Partner.new(id: '51213be7-c8de-4e06-8cc2-06bfc82cdd68',
+                                      code: 'bar',
+                                      rel_path: 'd/e/f')
+          end
+          subject { partner }
 
-      its(:save) { should eq true }
+          its(:save) { should eq true }
 
-      context "after save" do
-        before { partner.save }
-        its(:id)         { should eq '51213be7-c8de-4e06-8cc2-06bfc82cdd68' }
-        its(:code)       { should eq 'bar'   }
-        its(:created_at) { should_not be_nil }
-        its(:updated_at) { should_not be_nil }
+          context "after save" do
+            before { partner.save }
+            its(:id)         { should eq '51213be7-c8de-4e06-8cc2-06bfc82cdd68' }
+            its(:code)       { should eq 'bar'   }
+            its(:created_at) { should_not be_nil }
+            its(:updated_at) { should_not be_nil }
+          end
+        end
       end
     end
   end
