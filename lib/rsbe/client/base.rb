40334707a70fd251f6febb16a66aa3f8ceea5c06
jgpawletko <jgpawletko@gmail.com>
2014-11-03 13:23:26 -0500

    refactor PATH constant

--- lib/rsbe/client/base.rb
+++ lib/rsbe/client/base.rb
@@ -5,15 +5,11 @@ module Rsbe
   module Client
     class Base
       def initialize
-        @user     = ENV['RSBE_USER']     || 'foo'
-        @password = ENV['RSBE_PASSWORD'] || 'bar'
-        @url      = ENV['RSBE_URL']      || 'http://localhost:3000'
-        @conn     = Faraday.new(url: @url) do |faraday|
-          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-        end
-        @conn.basic_auth(@user, @password)
+        @conn = Rsbe::Client::Connection.new
       end
-      PATH = '/api/v0'
+      def base_path
+        '/api/v0'
+      end
     end
   end
 end
--- spec/rsbe/client/base_spec.rb
+++ spec/rsbe/client/base_spec.rb
@@ -1,6 +1,7 @@
 describe Rsbe::Client::Base do
   context "when created" do
-    subject    { Rsbe::Client::Base.new }
-    it         { should be_a(Rsbe::Client::Base) }
+    subject { Rsbe::Client::Base.new }
+    it      { should be_a(Rsbe::Client::Base) }
+    its(:base_path) { should == '/api/v0' }
   end
 end
