7f41e3fa0121a765dceb0c0ad25c6202cdcce4e4
jgpawletko <jgpawletko@gmail.com>
2015-08-09 23:01:11 -0400

    add Provider.all

--- spec/rsbe/client/provider_spec.rb
+++ spec/rsbe/client/provider_spec.rb
@@ -12,8 +12,21 @@ describe Rsbe::Client::Provider do
       end
     end
   end
+
   describe ".base_path" do
     subject { Rsbe::Client::Provider }
     its(:base_path) { should eq '/api/v0/providers' }
+  end
+
+  describe ".all", vcr: {cassette_name: 'provider/all'} do
+    context "returned array" do
+      subject { Rsbe::Client::Provider.all }
+      it         { should be_a(Array) }
+      its(:size) { should eq 4 }
+    end
+    context "returned array element" do
+      subject { Rsbe::Client::Provider.all[0] }
+      it { should be_a(Rsbe::Client::Provider) }
+    end
   end
 end
