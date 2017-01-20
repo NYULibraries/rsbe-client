ac43da44ef6d92045d733ee16f4be9d20861c16e
jgpawletko <jgpawletko@gmail.com>
2017-01-20 18:41:59 -0500

    add vcr cassettes for Rsbe::Client.find

--- spec/rsbe/client_spec.rb
+++ spec/rsbe/client_spec.rb
@@ -1,34 +1,34 @@
 describe Rsbe::Client do
-  PARTNER_URL    = 'http://localhost:3000/api/v0/partners/2f096796-c685-444f-a4fe-5971346b159d'
-  COLLECTION_URL = 'http://localhost:3000/api/v0/colls/06347ad0-1bdb-433e-849b-f3744a4f2eed'
+  PARTNER_URL    = 'http://localhost:3000/api/v0/partners/977e659b-886a-4626-8799-8979426ad2b3'
+  COLLECTION_URL = 'http://localhost:3000/api/v0/colls/ea85c776-a79b-4603-b307-d6760a400281'
   PROVIDER_URL   = 'http://localhost:3000/api/v0/providers/51213be7-c8de-4e06-8cc2-06bfc82cdd68'
   SE_URL         = 'http://localhost:3000/api/v0/ses/acf51ef2-8bf3-4f05-9042-0bfCB6860560'
   NO_OBJECT_URL  = 'http://localhost:3000/api/v0/fluffy_bunnies/4bf6daf0-e065-46e2-af30-3f74b1f7478e' # does not exist
 
   describe '.find' do
     context 'when object exists' do
-      context 'and URL points to a Partner' do
+      context 'and URL points to a Partner', vcr: { cassette_name: 'client/find_partner' }  do
         subject { Rsbe::Client.find(PARTNER_URL) }
         it { should be_a(Rsbe::Client::Partner) }
-        its(:id) { should == '2f096796-c685-444f-a4fe-5971346b159d' }
-        its(:code) { should == 'fales' }
+        its(:id) { should == '977e659b-886a-4626-8799-8979426ad2b3' }
+        its(:code) { should == 'foo' }
       end
 
-      context 'and URL points to a Collection' do
+      context 'and URL points to a Collection', vcr: { cassette_name: 'client/find_collection' } do
         subject { Rsbe::Client.find(COLLECTION_URL) }
         it { should be_a(Rsbe::Client::Collection) }
-        its(:id) { should == '06347ad0-1bdb-433e-849b-f3744a4f2eed' }
-        its(:code) { should == 'alba_audio018' }
+        its(:id) { should == 'ea85c776-a79b-4603-b307-d6760a400281' }
+        its(:code) { should == 'quux' }
       end
 
-      context 'and URL points to a Provider' do
+      context 'and URL points to a Provider', vcr: { cassette_name: 'client/find_provider' } do
         subject { Rsbe::Client.find(PROVIDER_URL) }
         it { should be_a(Rsbe::Client::Provider) }
         its(:id) { should == '51213be7-c8de-4e06-8cc2-06bfc82cdd68' }
         its(:name) { should == 'Calpurnia' }
       end
 
-      context 'and URL points to an Se' do
+      context 'and URL points to an Se', vcr: { cassette_name: 'client/find_se' } do
         subject { Rsbe::Client.find(SE_URL) }
         it { should be_a(Rsbe::Client::Se) }
         its(:id) { should == 'acf51ef2-8bf3-4f05-9042-0bfcb6860560' }
