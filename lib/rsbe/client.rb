d8884b365711ffb59a086af0e93b2bdcf1b6b950
jgpawletko <jgpawletko@gmail.com>
2017-01-20 15:58:36 -0500

    add Rsbe::Client::Connection#same_origin?

--- lib/rsbe/client.rb
+++ lib/rsbe/client.rb
@@ -1,9 +1,26 @@
-%w(base partner collection se connection not_found_error
-method_not_implemented_error provider).each do |r|
+%w(base
+partner
+collection
+provider
+se
+connection not_found_error
+method_not_implemented_error
+wrong_origin_error).each do |r|
   require_relative "./client/#{r}"
 end
 
 module Rsbe
   module Client
+    def self.find(url)
+      assert_same_origin(url)
+    end
+
+    def self.assert_same_origin(url)
+      conn = Rsbe::Client::Connection.new
+      unless conn.same_origin?(url)
+        raise Rsbe::Client::WrongOriginError.new("incorrect origin: #{url}")
+      end
+    end
+    private_class_method :assert_same_origin
   end
 end
---
+++ lib/rsbe/client/wrong_origin_error.rb
@@ -0,0 +1,6 @@
+module Rsbe
+  module Client
+    class WrongOriginError < RuntimeError
+    end
+  end
+end
