cd71ac261cbf7f817faacf533497d52b6e5e64f1
jgpawletko <jgpawletko@gmail.com>
2017-01-20 10:27:33 -0500

    add Rsbe::Client.find test plan

---
+++ spec/rsbe/client_spec.rb
@@ -0,0 +1,19 @@
+describe Rsbe::Client do
+  describe '.find' do
+    context 'when object exists' do
+      pending 'and URL points to a Partner'
+      pending 'and URL points to a Collection'
+      pending 'and URL points to a Provider'
+      pending 'and URL points to an Se'
+    end
+
+    context 'when object does not exist' do
+      pending 'it should return nil'
+    end
+
+    context 'when hostname does not match RSBE_URL' do
+      pending 'it should raise an exception'
+    end
+  end
+end
+
