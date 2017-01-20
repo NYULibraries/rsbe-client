833211b00c969e6fbf8e2c375e6755322d024d5a
jgpawletko <jgpawletko@gmail.com>
2017-01-20 18:21:55 -0500

    add Rsbe::Client.find implmementation

--- lib/rsbe/client.rb
+++ lib/rsbe/client.rb
@@ -5,7 +5,8 @@ provider
 se
 connection not_found_error
 method_not_implemented_error
-wrong_origin_error).each do |r|
+wrong_origin_error
+unrecognized_resource_error).each do |r|
   require_relative "./client/#{r}"
 end
 
@@ -13,6 +14,7 @@ module Rsbe
   module Client
     def self.find(url)
       assert_same_origin(url)
+      find_and_instantiate(url)
     end
 
     def self.assert_same_origin(url)
@@ -22,5 +24,24 @@ module Rsbe
       end
     end
     private_class_method :assert_same_origin
+
+    def self.find_and_instantiate(url)
+      retval = nil
+      [ 
+        Rsbe::Client::Partner, 
+        Rsbe::Client::Provider,
+        Rsbe::Client::Collection, 
+        Rsbe::Client::Se
+      ].each do |klass|
+        # UUID Regexp from http://stackoverflow.com/a/14166194
+        m = /#{klass.base_path}\/(?<uuid>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})\z/i.match(url)
+        next if m.nil?
+        retval = klass.find(m['uuid'])
+        break
+      end
+      raise Rsbe::Client::UnrecognizedResourceError.new("no matching resource found for #{url}") if retval.nil?
+      retval
+    end
+    private_class_method :find_and_instantiate
   end
 end
--- spec/rsbe/client_spec.rb
+++ spec/rsbe/client_spec.rb
@@ -1,33 +1,51 @@
 describe Rsbe::Client do
+  PARTNER_URL    = 'http://localhost:3000/api/v0/partners/2f096796-c685-444f-a4fe-5971346b159d'
+  COLLECTION_URL = 'http://localhost:3000/api/v0/colls/06347ad0-1bdb-433e-849b-f3744a4f2eed'
+  PROVIDER_URL   = 'http://localhost:3000/api/v0/providers/51213be7-c8de-4e06-8cc2-06bfc82cdd68'
+  SE_URL         = 'http://localhost:3000/api/v0/ses/acf51ef2-8bf3-4f05-9042-0bfCB6860560'
+  NO_OBJECT_URL  = 'http://localhost:3000/api/v0/fluffy_bunnies/4bf6daf0-e065-46e2-af30-3f74b1f7478e' # does not exist
+
   describe '.find' do
     context 'when object exists' do
       context 'and URL points to a Partner' do
-        pending 'it should return a Partner object'
-        pending 'the Partner object should have the correct id'
+        subject { Rsbe::Client.find(PARTNER_URL) }
+        it { should be_a(Rsbe::Client::Partner) }
+        its(:id) { should == '2f096796-c685-444f-a4fe-5971346b159d' }
+        its(:code) { should == 'fales' }
       end
 
       context 'and URL points to a Collection' do
-        pending 'it should return a Collection object'
-        pending 'the Collection object should have the correct id'
+        subject { Rsbe::Client.find(COLLECTION_URL) }
+        it { should be_a(Rsbe::Client::Collection) }
+        its(:id) { should == '06347ad0-1bdb-433e-849b-f3744a4f2eed' }
+        its(:code) { should == 'alba_audio018' }
       end
 
       context 'and URL points to a Provider' do
-        pending 'it should return a Provider object'
-        pending 'the Provider object should have the correct id'
+        subject { Rsbe::Client.find(PROVIDER_URL) }
+        it { should be_a(Rsbe::Client::Provider) }
+        its(:id) { should == '51213be7-c8de-4e06-8cc2-06bfc82cdd68' }
+        its(:name) { should == 'Calpurnia' }
       end
 
       context 'and URL points to an Se' do
-        pending 'it should return an Se object'
-        pending 'the Se object should have the correct id'
+        subject { Rsbe::Client.find(SE_URL) }
+        it { should be_a(Rsbe::Client::Se) }
+        its(:id) { should == 'acf51ef2-8bf3-4f05-9042-0bfcb6860560' }
+        its(:digi_id) { should == 'foo_quux_cuid01234' }
       end
     end
 
-    context 'when object does not exist' do
-      pending 'it should return nil'
+    context 'when URL does not match an existing rsbe resource' do
+      it 'should raise an exception' do
+        expect { Rsbe::Client.find(NO_OBJECT_URL) }.to raise_error Rsbe::Client::UnrecognizedResourceError
+      end
     end
 
-    context 'when hostname does not match RSBE_URL' do
-      pending 'it should raise an exception'
+    context 'when URL origin does not match the current connection configuration' do
+      it 'should raise an exception' do
+        expect { Rsbe::Client.find('some-bogus-url') }.to raise_error Rsbe::Client::WrongOriginError
+      end
     end
   end
 end
