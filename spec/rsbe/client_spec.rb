cc5457d550c869f6064eddec2d03357ef855be82
jgpawletko <jgpawletko@gmail.com>
2017-01-20 11:32:06 -0500

    refine Rsbe::Client.find test plan

--- spec/rsbe/client_spec.rb
+++ spec/rsbe/client_spec.rb
@@ -1,10 +1,25 @@
 describe Rsbe::Client do
   describe '.find' do
     context 'when object exists' do
-      pending 'and URL points to a Partner'
-      pending 'and URL points to a Collection'
-      pending 'and URL points to a Provider'
-      pending 'and URL points to an Se'
+      context 'and URL points to a Partner' do
+        pending 'it should return a Partner object'
+        pending 'the Partner object should have the correct id'
+      end
+
+      context 'and URL points to a Collection' do
+        pending 'it should return a Collection object'
+        pending 'the Collection object should have the correct id'
+      end
+
+      context 'and URL points to a Provider' do
+        pending 'it should return a Provider object'
+        pending 'the Provider object should have the correct id'
+      end
+
+      context 'and URL points to an Se' do
+        pending 'it should return an Se object'
+        pending 'the Se object should have the correct id'
+      end
     end
 
     context 'when object does not exist' do
