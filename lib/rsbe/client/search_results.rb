a5838480ff3b2b014ffd9cb0e1949fbf1285a603
jgpawletko <jgpawletko@gmail.com>
2017-07-05 23:21:12 +0000

    add SearchResults#num_found, refactor spec

--- lib/rsbe/client/search_results.rb
+++ lib/rsbe/client/search_results.rb
@@ -3,10 +3,15 @@ module Rsbe
     class SearchResults
       def initialize(response)
         @response = response
+        @json     = JSON.parse(@response.body)
       end
 
       def success?
         @response.status == 200
+      end
+
+      def num_found
+        @json['response']['numFound']
       end
     end
   end
--- spec/rsbe/client/search_results_spec.rb
+++ spec/rsbe/client/search_results_spec.rb
@@ -1,27 +1,34 @@
 require 'ostruct'
 
 describe Rsbe::Client::SearchResults do
-  describe '.new' do
-    it 'should return the correct class' do
-      expect(Rsbe::Client::SearchResults.new({}).class).to eq(Rsbe::Client::SearchResults)
-    end
-  end
-
-  context "with valid search parameters" do
-    valid_params = { params: { 
+  let(:valid_params) {  { 
+      params: { 
         coll_id: "ea85c776-a79b-4603-b307-d6760a400281",
         digi_id: "foo_quux_cuid370"
       },
       required_params: [],
       scope: "ses"
     }
+  }
+  let(:response) { Rsbe::Client::Search.search(valid_params) }
+  let(:sut) { Rsbe::Client::SearchResults.new(response) }
 
-    let(:response) { Rsbe::Client::Search.search(valid_params) }
-    describe '.success?' do
-      let(:sut) { Rsbe::Client::SearchResults.new(response) }
-    
-      it 'should return true for a valid search' do
+  describe '.new' do
+    it 'should return the correct class' do
+      expect(sut.class).to eq(Rsbe::Client::SearchResults)
+    end
+  end
+
+  context "with valid search parameters" do
+    describe '#success?' do
+      it 'should return true' do
         expect(sut.success?).to eq(true)
+      end
+    end
+
+    describe '#num_found' do
+      it 'should return the correct count' do
+        expect(sut.num_found).to eq(1)
       end
     end
   end
