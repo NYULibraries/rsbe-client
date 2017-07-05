d79bff364e99b385ca72ea9f7f46e16eb0ad8750
jgpawletko <jgpawletko@gmail.com>
2017-07-05 22:11:22 +0000

    tweak spec to use actual Search response

--- spec/rsbe/client/search_results_spec.rb
+++ spec/rsbe/client/search_results_spec.rb
@@ -1,18 +1,28 @@
 require 'ostruct'
 
 describe Rsbe::Client::SearchResults do
-  let(:response) { OpenStruct.new(status: 200) }
-  let(:sut) { Rsbe::Client::SearchResults.new(response) }
-
   describe '.new' do
     it 'should return the correct class' do
-      expect(sut.class).to eq(Rsbe::Client::SearchResults)
+      expect(Rsbe::Client::SearchResults.new({}).class).to eq(Rsbe::Client::SearchResults)
     end
   end
 
-  describe '.success?' do
-    it 'should return true for a valid search' do
-      expect(sut.success?).to eq(true)
+  context "with valid search parameters" do
+    valid_params = { params: { 
+        coll_id: "ea85c776-a79b-4603-b307-d6760a400281",
+        digi_id: "foo_quux_cuid370"
+      },
+      required_params: [],
+      scope: "ses"
+    }
+
+    let(:response) { Rsbe::Client::Search.search(valid_params) }
+    describe '.success?' do
+      let(:sut) { Rsbe::Client::SearchResults.new(response) }
+    
+      it 'should return true for a valid search' do
+        expect(sut.success?).to eq(true)
+      end
     end
   end
 end
