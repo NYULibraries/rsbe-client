b889293df2a24405f23ac5aa6e4686dda9d18670
jgpawletko <jgpawletko@gmail.com>
2017-07-05 23:34:52 +0000

    snapshot WIP: add memoization

--- lib/rsbe/client/search_results.rb
+++ lib/rsbe/client/search_results.rb
@@ -1,17 +1,28 @@
 module Rsbe
   module Client
     class SearchResults
-      def initialize(response)
-        @response = response
-        @json     = JSON.parse(@response.body)
+      def initialize(api_response)
+        @api_response = api_response
+        # init json
+        json
       end
 
       def success?
-        @response.status == 200
+        @api_response.status == 200
       end
 
       def num_found
-        @json['response']['numFound']
+        response['numFound']
+      end
+
+      private
+
+      def response
+        @response ||= json['response']
+      end
+
+      def json
+        @json ||= JSON.parse(@api_response.body)
       end
     end
   end
